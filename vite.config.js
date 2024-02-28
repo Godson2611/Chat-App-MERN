9a8fa5f750befc650fb68d6f6e13435a829384cd
Godson2611 <godson2611@gmail.com>
2024-02-28 23:21:45 +0530

    add dotenv

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    port:3000,
-    proxy:{
-      "/api":{
-        target: "http://localhost:8000",
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: `${import.meta.env.VITE_URL}`,
       },
     },
   },
-})
+});
