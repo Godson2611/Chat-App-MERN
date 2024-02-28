ccbb777bafc2ba9c0ded7625c873e794729e8b04
Godson2611 <godson2611@gmail.com>
2024-02-29 00:26:42 +0530

    changes in vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: `${import.meta.env.VITE_URL}`,
+        target: `http://localhost:8000`,
       },
     },
   },
