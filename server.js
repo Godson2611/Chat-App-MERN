c9385e081eed04c407cf595467dd0b2103060d8e
Godson2611 <godson2611@gmail.com>
2024-03-06 22:46:28 +0530

    correct the port number

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import connectToMongoDB from "./database/connectToMongoDB.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json())
 app.use(cookieParser())
