aa9f29b8065bf795eec8c7f66a32f93197fb6f51
Godson2611 <godson2611@gmail.com>
2024-03-07 10:46:56 +0530

    add cros

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import {app, server} from "./socket/socket.js"
+import cors from "cors";
+import { app, server } from "./socket/socket.js";
 
 import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
@@ -13,12 +14,13 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
-app.use('/api/message', messageRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/message', messageRoutes);
+app.use('/api/user', userRoutes);
 
 server.listen(PORT, async () => {
     try {
